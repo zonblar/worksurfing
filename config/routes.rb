2ec1c30e09212eb56658a4599b514073ececea98
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-15 16:26:36 +0100

    First test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, only: [:show, :new, :create, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

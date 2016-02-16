735387ae0cfbdbfc1d0dcacf56a9b540cd37ca47
Matthias Richard <matthias.rms@gmail.com>
2016-02-16 10:24:48 +0100

    booking basic logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,12 +3,19 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
     @workspace = Workspace.find(params[:workspace_id])
-    @booking = @workspace.bookings.new(booking_params)
-    @booking.save
-
-    redirect_to booking_path(@booking)
+    @booking = Booking.new(booking_params)
+    @booking.workspace_id = @workspace.id
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
-
+resources :bookings
   resources :users, only: [:show, :new, :create, :edit, :update]
   authenticate :user do
     resources :workspaces, only: [:new, :create, :edit, :update, :show] do
-    resources :bookings, only: [:index]
+    resources :bookings, only: [:index, :create]
   end
 end
 
-resources :bookings
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

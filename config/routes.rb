975a87f35906fd1dd6950eaed0bd73d3054cbd04
Matthias Richard <matthias.rms@gmail.com>
2016-02-16 11:08:57 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
-resources :bookings
-  resources :users, only: [:show, :new, :create, :edit, :update]
-  authenticate :user do
-    resources :workspaces, only: [:new, :create, :edit, :update, :show] do
-    resources :bookings, only: [:index, :create]
+  resources :bookings
+    resources :users, only: [:show, :new, :create, :edit, :update]
+    authenticate :user do
+      resources :workspaces, only: [:new, :create, :edit, :update, :show] do
+      resources :bookings, only: [:index, :create]
+    end
   end
-end
 end

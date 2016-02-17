0ee77b28a988dc0e64c463f5b0d0c2786c1b270f
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-17 14:58:49 +0100

    kevin input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :job, :portfolio, :description, :gender)
+    params.require(:user).permit(:first_name, :last_name, :email, :job, :portfolio, :description, :gender, :picture)
   end
 
   def set_user

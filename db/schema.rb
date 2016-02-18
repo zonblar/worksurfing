c52eb29c2700158a7ff8fa00c830bc5cfd8a6dbc
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-18 18:51:22 +0100

    admin added

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,12 @@ ActiveRecord::Schema.define(version: 20160218164039) do
     t.boolean  "accepted"
     t.integer  "workspace_id"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string   "state"
+    t.integer  "amount_cents",  default: 0, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "workspace_sku"
+    t.json     "payment"
   end
 
   add_index "bookings", ["user_id"], name: "index_bookings_on_user_id", using: :btree
@@ -119,18 +123,19 @@ ActiveRecord::Schema.define(version: 20160218164039) do
   add_index "reviews", ["workspace_id"], name: "index_reviews_on_workspace_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.boolean  "admin",                  default: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "job"
@@ -160,8 +165,10 @@ ActiveRecord::Schema.define(version: 20160218164039) do
     t.integer  "price_per_week"
     t.string   "type_of_space"
     t.integer  "user_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "price_cents",    default: 0, null: false
+    t.string   "sku"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "title"
     t.string   "photo1"
     t.float    "latitude"

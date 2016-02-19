7a37c60645c5beda3862b3d08d3d1df05be10cc1
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-19 10:04:22 +0100

    Adding a status field

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160218201411) do
-
+ActiveRecord::Schema.define(version: 20160219090310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160218201411) do
     t.datetime "updated_at",                null: false
     t.string   "workspace_sku"
     t.json     "payment"
+    t.string   "status"
   end
 
   add_index "bookings", ["user_id"], name: "index_bookings_on_user_id", using: :btree
@@ -125,18 +124,19 @@ ActiveRecord::Schema.define(version: 20160218201411) do
   add_index "reviews", ["workspace_id"], name: "index_reviews_on_workspace_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.boolean  "admin",                  default: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "job"
